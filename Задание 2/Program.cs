6a4a02b119619214245250f1c52fa7fef7fe504c
Kinder <kindermaks@mail.ru>
2022-10-16 20:16:38 +0300

    4

--- Задание 2/Program.cs
+++ Задание 2/Program.cs
@@ -37,8 +37,44 @@ Console.WriteLine("Max = " + max);
 Console.WriteLine("Input number ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 2 == 1)
-    Console.WriteLine("odd integer");
-else
+if (a % 2 == 0)
     Console.WriteLine("even integer");
+else
+    Console.WriteLine("odd integer");
 */
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*Console.WriteLine("Input number ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = -1;
+
+N = (N % 2 == 1)
+{
+   
+    N = N - 1;
+}
+while (N <= i)
+
+i++;
+Console.WriteLine(N);
+*/
+
+Console.WriteLine("Input number ");
+int N =  Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int k = 1;
+while (true)
+{
+    if (N % 2 == 0)
+    {
+        Console.Write(N + " ");
+    }
+    if (k < N)
+    {
+        break;
+    }
+    i++;
+}
+
+
