07a20c0210840dffb8f012d128c362374245e393
Kinder <kindermaks@mail.ru>
2022-10-19 13:39:32 +0300

    1

---
+++ Home Work 2/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*
+int CutNumber(int number)
+{
+    int FirstNum = number / 10;
+    int SecondNumber = FirstNum % 10;
+
+    number = SecondNumber;
+    return number;
+}
+int num = new Random().Next(100, 1000);
+
+int result = CutNumber(num);
+
+Console.WriteLine($"New version {num} is {result}");
+*/
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
