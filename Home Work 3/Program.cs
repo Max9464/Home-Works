6722ad9aecb257b82633d35f1c106b26d860acdf
Kinder <kindermaks@mail.ru>
2022-10-25 16:32:10 +0300

    3

---
+++ Home Work 3/Program.cs
@@ -0,0 +1,35 @@
+﻿bool Pol(int number)
+{
+    int a = number % 10;
+    int b = (number % 100) * 10 + a;
+    int c = (number % 1000) * 100 + b;
+    int d = (number % 10000) * 1000 + c;
+    int f = (number % 100000) * 10000 + d;
+    
+        if (number == f)
+     {
+        return true;
+     }
+    
+        else
+     {
+         return false;
+     }
+    
+}
+
+//int num = new Random().Next(10000, 1000000);
+Console.Write("Number ");
+int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"{num}");
+bool result = Pol(num);
+
+    if(result == true)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
