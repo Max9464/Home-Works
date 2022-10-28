01bd6b9db13372034ebc0b96f0b28bc1387abd36
Kinder <kindermaks@mail.ru>
2022-10-28 14:00:45 +0300

    7

--- Home Work 3/Program.cs
+++ Home Work 3/Program.cs
@@ -1,4 +1,6 @@
-﻿bool Pol(int number)
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+bool Pol(int number)
 {
     int a = number % 10;
     int b = (number / 10) % 10;
@@ -16,19 +18,42 @@
     {
         return false;
     }
-    
+
 }
 
-Console.Write("Number ");
+Console.Write("Input five's number ");
 int num = Convert.ToInt32(Console.ReadLine());
 bool result = Pol(num);
-    if (result == true)
+if (result == true)
 {
     Console.WriteLine("Да");
 }
-    else
+else
 {
     Console.WriteLine("Нет");
-    
 
 }
+*/
+
+
+double Range(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
+    return distance;
+}
+Console.WriteLine("input First Point x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input Second Point x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double lenght = Range(x1, y1, x2, y2, z1, z2);
+Console.WriteLine($"Distance between tow points is {Math.Round(lenght, 2)}");
+
+
