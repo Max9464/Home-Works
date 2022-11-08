84115688c2d0625259bfc459f6435e0af6ae5eae
Max9464 <kindermaks@mail.ru>
2022-11-08 12:48:08 +0300

    1

---
+++ Home Work 4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Num(int a, int b)
+{
+    int current = 1;
+    while ( current != b)
+    {
+        a = a * a;
+        current++;
+    }
+    
+    return a;
+
+}
+Console.Write("Input firts number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Num(a, b));
