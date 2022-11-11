ff3f07721dbc10d47ea753ebde04970d484b39ac
Max9464 <kindermaks@mail.ru>
2022-11-11 15:17:01 +0300

    17

--- Home Work 4/Program.cs
+++ Home Work 4/Program.cs
@@ -21,25 +21,25 @@ Console.WriteLine(Num(a, b));
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-/*Console.WriteLine("Input a number: ");
+Console.WriteLine("Input a number: ");
 int i = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 
 while (i > 0)
 {
-int num = i % 10;
-i = i / 10;
-sum = sum + num;
+int num = i % 10; // 4, 6, 5,3
+i = i / 10; //2356// 235, 23,2
+sum = sum + num; //0 + 4 sum = 4,  4+6=10,10+5=15, 15+3=18, 
 }
 Console.WriteLine("Sum is: " + sum);
-*/
+
 
 // Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-int[] array= new int[8] {1,5,9,8,7,9,1,3};
+/*/int[] array= new int[8] {1,5,9,8,7,9,1,3};
 
 for (int i = 0; i < array.Length; i++)
 {
     Console.Write(array[i]);
 }
-
+*/
---
+++ Home Work 5/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+/*int[] CreateRandommArray(int size) 
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+
+        array[i] = new Random().Next(100, 1000);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+
+}
+
+int EvenSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0) 
+    sum ++ ;
+
+return sum;
+
+}
+
+Console.Write("Input of elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int[] myArray = CreateRandommArray(n);
+ShowArray(myArray);
+
+int evenSum = EvenSum(myArray);
+Console.WriteLine($"Even sum is :" + evenSum);
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+
+
+
+
+
+
+
